987c782c5fc7650622a89af653790bcad4e89924
Max Thunyakij <maxthunyakij@gmail.com>
2014-04-12 18:10:07 -0700

    initial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,7 @@
+class HomeController < ApplicationController
+
+  def index
+   render :json => "Login Success!"
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+ 
+  devise_for :user
+  root to: "home#index"
+  
+end
